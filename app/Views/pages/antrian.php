05926554c3d1acf663612e320c93df5fa2fbd74e
Tangker17 <ramon.misterhadi@gmail.com>
2022-07-13 18:40:10 +0700

    first commit

---
+++ app/Views/pages/antrian.php
@@ -0,0 +1,61 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="section">
+    <div class="container">
+<iframe width="300" height="300px"
+src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnRqo_of0erFLEuALXkgIVRCXeTBdsQijwDw&usqp=CAU"></iframe>
+
+    <br>
+        <aside id="sidebar">
+
+    <div class="widget-box">
+        <center>
+            <h5 class="title"></h5>
+            <h1>
+                A1
+            </h1>
+            
+        <button class="btn btn-warning">Ruangan 1</button>
+        </center>
+       
+
+            </div>
+
+    <div class="widget-box">
+        <center>
+            <h5 class="title"></h5>
+            <h1>
+                A2
+            </h1>
+            
+        <button class="btn btn-warning">Ruangan 2</button>
+        </center>
+        <div class="widget-box">
+        <center>
+            <h5 class="title"></h5>
+            <h1>
+                A3
+            </h1>
+            
+        <button class="btn btn-warning">Ruangan 3</button>
+        </center>
+        <center>
+        <div class="widget-box">
+    <h5 class="title"></h5>
+    <h1>
+        A4
+        </h1>
+
+     <button class="btn btn-warning">Ruangan 4</button>
+</center>
+</aside>
+</div>
+</div>
+
+        </div>
+    </div>
+    <br>
+    <br>
+    <?= $this->endSection(); ?>
